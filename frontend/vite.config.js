2138fc66934c4f07203059dc967d7728e981723b
Baiju Kuamr Yadav <yadavbaiju2005@gmail.com>
2025-10-30 01:59:31 +0530

    Enhance Vite config with build and server settings
    Added build and server configurations to Vite.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, 
+  },
+  preview: {
+    port: 4173, 
+    strictPort: true,
+  },
+
+  resolve: {
+    alias: {},
+  },
 })
