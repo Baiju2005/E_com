19a5e8cfcda99ebe7c20853b3af07f3ae5135132
Baiju Kuamr Yadav <yadavbaiju2005@gmail.com>
2025-10-30 02:22:15 +0530

    Remove build, server, and preview configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true, 
-  },
-  preview: {
-    port: 4173, 
-    strictPort: true,
-  },
-
-  resolve: {
-    alias: {},
-  },
+  base: '/',
 })
